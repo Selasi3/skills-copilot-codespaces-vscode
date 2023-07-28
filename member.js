0c9831616165f671e713f5e0a1cf094d7568df18
Selasi Ayittah <selasiayittah3@gmail.com>
2023-07-28 04:31:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: "E",
+        templateUrl: 'modules/skills/views/member.html',
+        controller:"SkillMemberController",
+        controllerAs: "vm",
+        bindToController: true,
+        scope:{
+            member:'m'
+        }
+
+    }
+}
